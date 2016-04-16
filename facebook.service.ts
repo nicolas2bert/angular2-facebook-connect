0e66bdb7b7961eac0696abb87c3283338817e5d5
Nicolas HUMBERT <nicolas.humbert@live.fr>
2016-04-15 19:09:02 -0700

    Create facebook.service.ts

---
+++ facebook.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class FacebookService {
+
+  constructor() {
+    if (!window.fbAsyncInit) {
+      console.log('define');
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId: "your-app-id",
+          xfbml: true,
+          version: 'v2.5'
+        });
+      };
+    }
+  }
+
+  loadAndInitFBSDK():Observable{
+    var js,
+      id = 'facebook-jssdk',
+      ref = document.getElementsByTagName('script')[0];
+
+    if (document.getElementById(id)) {
+      return;
+    }
+
+    js = document.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+
+    ref.parentNode.insertBefore(js, ref);
+  }
+
+}
