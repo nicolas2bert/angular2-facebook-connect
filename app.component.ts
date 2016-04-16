1d03418bce22f77722db6278925c41a5c75fcec6
Nicolas HUMBERT <nicolas.humbert@live.fr>
2016-04-15 19:07:53 -0700

    Create app.component.ts

---
+++ app.component.ts
@@ -0,0 +1,33 @@
+
+import {Component, OnInit} from 'angular2/core';
+import {FacebookService} from './facebook/facebook.service';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/facebook.component.html',
+    styleUrls:  ['app/facebook.component.css'],
+    providers: [FacebookService]
+})
+export class AppComponent implements OnInit{
+
+  constructor(
+	  	private _facebookService: FacebookService
+	){}
+
+	ngOnInit(){
+    	this._facebookService.loadAndInitFBSDK();
+	}
+
+	login(){
+	    FB.login(function(response) {
+		    if (response.authResponse) {
+		     console.log('Welcome!  Fetching your information.... ');
+		     FB.api('/me', function(response) {
+		       console.log('Good to see you, ' + response.name + '.');
+		     });
+		    } else {
+		     console.log('User cancelled login or did not fully authorize.');
+		    }
+		});
+	}
+}
